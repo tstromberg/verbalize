b9028088b5d98651ed97de93eb78732d278aa35b
Thomas Stromberg <tstromberg@google.com>
2013-09-14 21:28:01 -0700

    Update ckeditor to 4.2.1

--- third_party/ckeditor/config.js
+++ third_party/ckeditor/config.js
@@ -1,26 +1,38 @@
-﻿/**
- * Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-  // customized for verbalize
-  config.toolbar = [[
-      'Source', '-' ,
+	// Define changes to default configuration here.
+	// For the complete reference:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-      'Format', 'Bold', 'Italic', 'Underline', 'StrikeThrough', '-',
-      'NumberedList','BulletedList', '-',
-      'Outdent', 'Indent','-',
-      'Blockquote','CreateDiv',
-    ],
-    ['TextColor', 'BGColor' ],
-    [ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock', '-',
-      'BidiLtr','BidiRtl'],
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
 
-    ['Link','Unlink','Anchor', 'Image', 'HorizontalRule',],
-    ['Find', 'Replace'],
-  ];
-  config.height = 800;
+	// Remove some buttons, provided by the standard plugins, which we don't
+	// need to have in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Se the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Make dialogs simpler.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
-
-
