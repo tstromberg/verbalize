889da797f55911dd70f928f6c955c4b25924709a
Thomas Stromberg <tstromberg@google.com>
2013-09-23 22:07:17 -0700

    Stop ckeditor from stripping div tags.

--- third_party/ckeditor/config.js
+++ third_party/ckeditor/config.js
@@ -35,4 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Make dialogs simpler.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+        // Enable divs.
+        config.allowedContent = true;
 };
