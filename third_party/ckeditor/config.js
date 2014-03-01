57f4bd2b0c3160440a5698e1d14998e53e409908
tstromberg <t@stromberg.org>
2014-03-01 15:13:41 -0800

    Update to ckeditor 4.3.3 - minified.

--- third_party/ckeditor/config.js
+++ third_party/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
@@ -35,8 +35,4 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Make dialogs simpler.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-
-        config.allowedContent = true;
-        config.extraPlugins = 'image2';
-        config.removePlugins = 'image,forms';
 };
